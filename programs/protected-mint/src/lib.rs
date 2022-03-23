31b76f9f4a60fbaf2f76404eb84ee93696eceaa9
throwbackjams <yli@alumni.nd.edu>
2022-03-23 16:54:05 -0500

    partial implementation of refund + cleanup of prev functions

--- programs/protected-mint/src/lib.rs
+++ programs/protected-mint/src/lib.rs
@@ -1,4 +1,7 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{TokenAccount, Mint};
+use mpl_token_metadata;
+use mpl_token_metadata::state::{Metadata, PREFIX, EDITION};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -44,20 +47,21 @@ pub mod protected_mint {
 
         let config_account = &mut ctx.accounts.config_account;
         
-        //Check if the Config Account's SOL balance has crossed the pre-determined threshold
         let available_lamports = **config_account.to_account_info().lamports.borrow(); 
-
-        if !((config_account.threshold_level < available_lamports) || (config_account.threshold_met)) {
-            return Err(ErrorCode::ThresholdNotMet.into());
-        }
 
         //Release available lamports less rent exemption (in case more mints occur after indicated time)
         //Note: can also config such that end_sales_time == candy machine's end time. If so, then can transfer all lamports and close account below
-        let creator_account = &ctx.accounts.creator_address;
         let space = 32 + 8 + 8 + 8 + 1 + 8 as usize; //Config Account: space = creator pubkey + sale price + quantity + threshold level + threshold bool + end sales time
         let rent = Rent::get()?;
         let rent_exempt_lamports = rent.minimum_balance(space);
-        let lamports_to_transfer = available_lamports.checked_sub(rent_exempt_lamports);
+        let lamports_to_transfer = available_lamports.checked_sub(rent_exempt_lamports).unwrap();
+        
+        //Check if the Config Account's available SOL balance has crossed the pre-determined threshold or if the threshold_met boolean is true
+        if !((config_account.threshold_level <= lamports_to_transfer) || (config_account.threshold_met)) {
+            return Err(ErrorCode::ThresholdNotMet.into());
+        }
+
+        let creator_account = &ctx.accounts.creator_address;
 
         let (_config_account, config_account_bump) =
             Pubkey::find_program_address(&[b"config-seed".as_ref(), ctx.accounts.creator_address.key.as_ref()], ctx.program_id);
@@ -71,7 +75,7 @@ pub mod protected_mint {
             &anchor_lang::solana_program::system_instruction::transfer(
                 config_account.to_account_info().key,
                 &config_account.creator_address,
-                lamports_to_transfer.unwrap(),
+                lamports_to_transfer,
             ),
             &[
                 config_account.to_account_info().clone(),
@@ -81,6 +85,7 @@ pub mod protected_mint {
 
         )?;
 
+        //Question: Is the PDA paying for the transfer transaction fee? Does this need to be accounted for in the avilable_lamports / lamports_to_transfer vars?
 
         // Note: Tried to use new Anchor wrappers functions for system program cpi calls but says Transfer not found in anchor_lang::system_program
         // let cpi_accounts = anchor_lang::system_program::Transfer{
@@ -107,6 +112,97 @@ pub mod protected_mint {
 
         Ok(())
     }
+
+    #[access_control(past_end_sales_time(&ctx.accounts.config_account, &ctx.accounts.clock))]
+    pub fn provide_refund(
+        ctx: Context<RequestRefund>
+    ) -> Result<()> {
+
+        //Check if the threshold level is met & if account has sufficient lamports to transfer
+        //TODO: Turn threshold check logic in provide_refund and release_funds into a function implemented on both structs
+        let config_account = &mut ctx.accounts.config_account;
+        
+        let available_lamports = **config_account.to_account_info().lamports.borrow(); 
+
+        //Available lamports excludes rent exemption
+        let space = 32 + 8 + 8 + 8 + 1 + 8 as usize; //Config Account: space = creator pubkey + sale price + quantity + threshold level + threshold bool + end sales time
+        let rent = Rent::get()?;
+        let rent_exempt_lamports = rent.minimum_balance(space);
+        let lamports_to_transfer = available_lamports.checked_sub(rent_exempt_lamports).unwrap();
+        
+        //Check if the Config Account's available SOL balance has crossed the pre-determined threshold or if the threshold_met boolean is true
+        if !((config_account.threshold_level < lamports_to_transfer) || (config_account.threshold_met)) {
+            return Err(ErrorCode::ThresholdNotMet.into());
+        }
+
+        if !(config_account.sale_price <= lamports_to_transfer) {
+            return Err(ErrorCode::InsufficientFunds.into());
+        }
+
+        //Check if signer user holds an NFT account, whose balance is 1, and whose token metadata updateAuthority matches the creator address in the Config
+        let nft_token_account = &ctx.accounts.nft_token_account;
+        let user = &ctx.accounts.user;
+        let nft_mint_account = &ctx.accounts.nft_mint;
+
+        //Check: token account is owned by the signer
+        assert_eq!(nft_token_account.owner, user.key());
+        //Check: token account mint corresponds to the mint account passed in
+        assert_eq!(nft_token_account.mint, nft_mint_account.key());
+        //Check: token account has quantity 1 (not empty)
+        assert_eq!(nft_token_account.amount, 1);
+
+        //Expect a Metaplex Master Edition so we derive the master_edition_key and compare to the mint passed to the program
+        let master_edition_seed = &[
+            PREFIX.as_bytes(),
+            ctx.accounts.token_metadata_program.key.as_ref(),
+            nft_token_account.mint.as_ref(),
+            EDITION.as_bytes()
+        ];
+
+        let (master_edition_key, _master_edition_seed) =
+            Pubkey::find_program_address(master_edition_seed, ctx.accounts.token_metadata_program.key);
+
+        assert_eq!(master_edition_key, ctx.accounts.nft_mint.key());
+
+        //Check that the metadata account derived from the passed-in NFT mint account matches the passed-in metadata account
+        let nft_metadata_account = &ctx.accounts.nft_metadata_account;
+        let nft_mint_account_pubkey = &ctx.accounts.nft_mint.key();
+
+        let metadata_seed = &[
+            "metadata".as_bytes(),
+            ctx.accounts.token_metadata_program.key.as_ref(),
+            nft_mint_account_pubkey.as_ref(),
+        ];
+
+        let (metadata_derived_key, _bump_seed) = 
+            Pubkey::find_program_address(metadata_seed, ctx.accounts.token_metadata_program.key);
+        
+        assert_eq!(metadata_derived_key, nft_metadata_account.key());
+
+        if ctx.accounts.nft_metadata_account.data_is_empty(){
+            return Err(ErrorCode::NFTMetadataEmpty.into());
+        }
+
+        let metadata_full_account = &mut Metadata::from_account_info(&ctx.accounts.nft_metadata_account)?;
+
+        let full_metadata_clone = metadata_full_account.clone();
+
+        let expected_creator = &ctx.accounts.config_account.creator_address;
+
+        //Check that the updateAuthority field on the NFT metadata matches the creator of the ProtectionConfig Account
+        assert_eq!(
+            full_metadata_clone.update_authority,
+            *expected_creator
+        );
+
+        //TODO: Signer burns the NFT (or sends it?) - probably use anchor_spl token burn?
+        
+        //TODO: Derive config account PDA and transfer sales_price to the user signer
+        
+
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
@@ -141,6 +237,23 @@ pub struct ReleaseFunds<'info>{
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct RequestRefund<'info>{
+    #[account(mut, signer)]
+    /// CHECK: Already checked as signer?
+    pub user: AccountInfo<'info>,
+    pub config_account: Account<'info, ProtectionConfig>,
+    pub nft_mint: Account<'info, Mint>,
+    pub nft_token_account: Account<'info, TokenAccount>,
+    /// CHECK: Refund function will derive the metadata key and ensure matches nft_metadata_account
+    pub nft_metadata_account: AccountInfo<'info>,
+    #[account(address = mpl_token_metadata::ID)]
+    /// CHECK: Already checked for address match?
+    pub token_metadata_program: AccountInfo<'info>,
+    pub clock: Sysvar<'info, Clock>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 pub struct ProtectionConfig {
     creator_address: Pubkey,
@@ -161,6 +274,10 @@ pub enum ErrorCode{
     SaleNotOver,
     #[msg("Pre-determined protection threshold is not yet met. Unable to trigger release of funds")]
     ThresholdNotMet,
+    #[msg("Insufficient Funds in Protection Vault. Unable to process refund")]
+    InsufficientFunds,
+    #[msg("NFT Metadata Account is empty")]
+    NFTMetadataEmpty,
 }
 
 fn future_end_time<'info>(ctx: &Context<InitProtectionConfig<'info>>, end_sales_time: i64) -> Result<()> {
